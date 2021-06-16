105745b7fe09bbbafdb11b568eb465787206fd84
Glowrare <aajibolanle@yahoo.com>
2021-06-16 08:14:06 +0100

    Update to dummy message and styling + function refactorings

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-const fetchDate = () => {
-  const today = new Date();
-  const dd = today.getDate();
-  let mm = today.getMonth() + 1;
-  if (mm < 10) {
-    mm = `0${mm}`;
-  }
-  const yyyy = today.getFullYear();
-  const date = `${dd}/${mm}/${yyyy}`;
-  return date;
-};
-const fetchTime = () => {
-  const today = new Date();
-  const hh = today.getHours();
-  let mm = today.getMinutes();
-  if (mm < 10) {
-    mm = `0${mm}`;
-  }
-  const time = `${hh}:${mm}`;
-  return time;
-};
+const sendBtn = document.querySelectorAll('.chattext-inputbtn');
+
+sendBtn.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    updateScreens(el);
+    scrolltoBottom();
+  });
+});
 
 const createElementWrapper = (el) => {
   return document.createElement(el);
@@ -30,14 +18,44 @@ const appendElementWrapper = (parent, el) => {
 const createLiTag = createElementWrapper('li');
 const createDivTag = createElementWrapper('div');
 
-const newChatDate = () => {
-  let newChatTag = createLiTag.cloneNode(true);
-  newChatTag.setAttribute('class', 'chatdate');
-  newChatTag.textContent = fetchDate();
-  return newChatTag;
+const updateScreens = (btn) => {
+  let sender = '';
+  let receiver = '';
+  if (btn.id === 'slToOQBtn') {
+    sender = 'olivers-screen';
+    receiver = 'saras-screen';
+  } else {
+    sender = 'saras-screen';
+    receiver = 'olivers-screen';
+  }
+
+  let chatInput = document
+    .querySelector(`.${sender} .chattext-inputbox`)
+    .value.trim();
+  let contactInitials = document.querySelector(
+    `.${receiver} .chat-header .initials`
+  );
+  let sentScreen = document.querySelector(`.${sender} .chatgroup`);
+  let receivedScreen = document.querySelector(`.${receiver} .chatgroup`);
+
+  let sentChat = createLiTag.cloneNode(true);
+  let recvdChat = createLiTag.cloneNode(true);
+
+  if (chatInput !== '') {
+    getChatMessageBody(contactInitials, sentChat, recvdChat, chatInput);
+    checkLastChatDate(sentScreen, receivedScreen);
+    appendElementWrapper(sentScreen, sentChat);
+    appendElementWrapper(receivedScreen, recvdChat);
+    document.querySelector(`.${sender} .chattext-inputbox`).value = '';
+  }
 };
 
-const getChatMessage = (contactInitials, sentChat, recvdChat, chatInput) => {
+const getChatMessageBody = (
+  contactInitials,
+  sentChat,
+  recvdChat,
+  chatInput
+) => {
   let initials = createDivTag.cloneNode(true);
   initials.setAttribute('class', 'initials');
   initials.textContent = contactInitials.textContent;
@@ -64,36 +82,34 @@ const getChatMessage = (contactInitials, sentChat, recvdChat, chatInput) => {
   appendElementWrapper(recvdChat, chatMessageWrapper.cloneNode(true));
 };
 
-const updateScreen = (btn) => {
-  let sender = '';
-  let receiver = '';
-  if (btn.id === 'slToOQBtn') {
-    sender = 'olivers-screen';
-    receiver = 'saras-screen';
-  } else {
-    sender = 'saras-screen';
-    receiver = 'olivers-screen';
+const fetchDate = () => {
+  const today = new Date();
+  const dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  if (mm < 10) {
+    mm = `0${mm}`;
   }
+  const yyyy = today.getFullYear();
+  const date = `${dd}/${mm}/${yyyy}`;
+  return date;
+};
 
-  let chatInput = document
-    .querySelector(`.${sender} .chattext-inputbox`)
-    .value.trim();
-  let contactInitials = document.querySelector(
-    `.${receiver} .chat-header .initials`
-  );
-  let sentScreen = document.querySelector(`.${sender} .chatgroup`);
-  let receivedScreen = document.querySelector(`.${receiver} .chatgroup`);
-
-  let sentChat = createLiTag.cloneNode(true);
-  let recvdChat = createLiTag.cloneNode(true);
-
-  if (chatInput !== '') {
-    getChatMessage(contactInitials, sentChat, recvdChat, chatInput);
-    checkLastChatDate(sentScreen, receivedScreen);
-    appendElementWrapper(sentScreen, sentChat);
-    appendElementWrapper(receivedScreen, recvdChat);
-    document.querySelector(`.${sender} .chattext-inputbox`).value = '';
+const fetchTime = () => {
+  const today = new Date();
+  const hh = today.getHours();
+  let mm = today.getMinutes();
+  if (mm < 10) {
+    mm = `0${mm}`;
   }
+  const time = `${hh}:${mm}`;
+  return time;
+};
+
+const newChatDate = () => {
+  let newChatTag = createLiTag.cloneNode(true);
+  newChatTag.setAttribute('class', 'chatdate');
+  newChatTag.textContent = fetchDate();
+  return newChatTag;
 };
 
 const checkLastChatDate = (sentScreen, receivedScreen) => {
@@ -107,14 +123,6 @@ const checkLastChatDate = (sentScreen, receivedScreen) => {
   }
 };
 
-const sendBtn = document.querySelectorAll('.chattext-inputbtn');
-sendBtn.forEach((el) => {
-  el.addEventListener('click', (e) => {
-    e.preventDefault();
-    updateScreen(el);
-    scrolltoBottom();
-  });
-});
 const scrolltoBottom = () => {
   let screenPos = document.querySelectorAll(`.chatwall`);
   screenPos.forEach((el) => {
